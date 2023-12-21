8e543c36fae765dc52e0882efd167145fc0b2b96
IgorLoureiro <igor.loureiro8@hotmail.com>
2023-12-20 21:41:20 -0300

    First commit Tech Blog

---
+++ src/app/data/fakedata.ts
@@ -0,0 +1,26 @@
+export const data = [
+    {
+        "id": "1",
+        "photo": "https://kinsta.com/pt/wp-content/uploads/sites/3/2020/04/ferramentas-de-revisao-de-codigo-1024x512.png",
+        "title": "Clean Code: O que é? Como aplicar?",
+        "description": "Clean code é uma abordagem de programação que enfatiza a clareza, simplicidade e legibilidade do código-fonte. Escrever código limpo não apenas facilita a compreensão do programa, mas também contribui para a manutenção eficiente, facilitando futuras modificações e correções de bugs. Princípios como nomes significativos de variáveis e funções, modularidade, coesão e baixa acoplamento são fundamentais para a criação de código limpo. Além disso, a adesão a convenções de estilo e a eliminação de duplicações desnecessárias são práticas comuns no desenvolvimento de código limpo. Em última análise, o clean code promove a qualidade do software, promovendo um ambiente de desenvolvimento colaborativo e sustentável."
+    },
+    {
+        "id": "2",
+        "photo": "https://hermes.digitalinnovation.one/articles/cover/1c5b2812-9fff-4aac-a17d-ec477fc5c058.jpg",
+        "title": "Nova Linguagem de Programação Promete Revolucionar Desenvolvimento de Software",
+        "description": "Python é uma linguagem de programação de alto nível, de propósito geral e de fácil leitura. Criada por Guido van Rossum, foi lançada em 1991 e se destaca por sua sintaxe clara e expressiva. É uma linguagem interpretada, suporta programação orientada a objetos, funcional e é amplamente utilizada em desenvolvimento web, ciência de dados, automação, inteligência artificial e muitas outras áreas. Python ganhou popularidade devido à sua versatilidade, vasta comunidade de desenvolvedores e extensa biblioteca padrão, tornando-a uma escolha popular para iniciantes e profissionais experientes."
+    },
+    {
+        "id": "3",
+        "photo": "https://hermes.dio.me/articles/cover/86932ce6-6e4e-4184-a20e-9f26a997c740.png",
+        "title": "Desenvolvedores Celebram Lançamento de Framework Inovador para Aplicações Web",
+        "description": "Angular é um framework de desenvolvimento front-end mantido pelo Google. Ele permite a criação de aplicativos web dinâmicos e escaláveis, utilizando a linguagem TypeScript. Angular segue o padrão de arquitetura MVVM (Model-View-ViewModel) e oferece uma estrutura robusta para a construção de interfaces de usuário interativas. Com recursos como two-way data binding, injeção de dependência e modularidade, Angular facilita o desenvolvimento, teste e manutenção de aplicações complexas. Além disso, o framework possui uma comunidade ativa, suporte a bibliotecas externas e integração com ferramentas populares, o que o torna uma escolha popular para o desenvolvimento de aplicações web modernas."
+    },
+    {
+        "id": "4",
+        "photo": "https://www.menosfios.com/wp-content/uploads/2023/11/google_stock.jpeg.jpg",
+        "title": "Empresa Líder Anuncia Atualização com Recursos Avançados em Sua Plataforma de Desenvolvimento",
+        "description": "A Google é uma gigante da tecnologia fundada em 1998 por Larry Page e Sergey Brin. Inicialmente conhecida por seu mecanismo de busca líder mundial, a empresa expandiu-se para oferecer uma ampla gama de serviços e produtos, incluindo sistemas operacionais (como o Android), aplicativos (como o Google Maps e o Gmail), hardware (como os smartphones Pixel e dispositivos Home), publicidade online e serviços em nuvem. A Google é reconhecida por sua inovação constante, pesquisa avançada, investimentos em inteligência artificial e pela sua influência significativa na indústria de tecnologia. A empresa é uma das maiores do mundo e desempenha um papel central na forma como as pessoas acessam informações e interagem online."
+    }
+]
